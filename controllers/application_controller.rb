e0c28c9689a493979b415c2944ef83d2c17c0abf
meganinch <megan.inch.09@aberdeen.ac.uk>
2014-05-19 15:09:06 +0100

    app

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -1,3 +1,35 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+
+before_action :set_locale
+ 
+def set_locale
+  I18n.locale = extract_locale_from_tld || I18n.default_locale
+end
+ 
+# Get locale from top-level domain or return nil if such locale is not available
+# You have to put something like:
+#   127.0.0.1 application.com
+#   127.0.0.1 application.it
+#   127.0.0.1 application.pl
+# in your /etc/hosts file to try this out locally
+def extract_locale_from_tld
+  parsed_locale = request.host.split('.').last
+  I18n.available_locales.include?(parsed_locale.to_sym) ? parsed_locale : nil
+end
+class Session < ActiveRecord::Base
+  def self.sweep(time = 1.hour)
+    if time.is_a?(String)
+      time = time.split.inject { |count, unit| count.to_i.send(unit) }
+    end
+ 
+	delete_all "updated_at < '#{time.ago.to_s(:db)}' OR
+  created_at < '#{2.days.ago.to_s(:db)}'"
+
+  end
+end
+
+
+
 end
